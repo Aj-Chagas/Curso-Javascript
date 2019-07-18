65e9f810bb8ab882572f7e1b2f47201519635048
Anderson Chagas <aj_chagas@hotmail.com>
2019-07-18 07:36:59 -0300

    for of

---
+++ EsNext/forOf.js
@@ -0,0 +1,17 @@
+for (let letras of 'Cod3r'){
+    console.log(letras)
+}
+
+const assuntosEcma = ['Map', 'Set', 'Promise']
+
+for(let i in assuntosEcma){
+    console.log(i)
+}
+
+for(let i of assuntosEcma){
+    console.log(i)
+}
+
+const assuntosMap = new Map({
+    ['Map', {}]
+})
