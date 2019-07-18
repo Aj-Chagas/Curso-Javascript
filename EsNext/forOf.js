8da732bb296ffa67d515e1acae827793e853e79f
Anderson Chagas <aj_chagas@hotmail.com>
2019-07-18 11:24:04 -0300

    for of(update)

--- EsNext/forOf.js
+++ EsNext/forOf.js
@@ -12,6 +12,29 @@ for(let i of assuntosEcma){
     console.log(i)
 }
 
-const assuntosMap = new Map({
-    ['Map', {}]
-})
+const assuntosMap = new Map([
+    ['Map', { abordado: true }],
+    ['Set', { abordado: true }],
+    ['Promise', { abordado: false }]
+])
+
+for (let assunto of assuntosMap){
+    console.log(assunto)
+}
+
+for(let chave of assuntosMap.keys()){
+    console.log('chave', chave)
+}
+
+for(let chave of assuntosMap.values()){
+    console.log('VALUES', chave)
+}
+
+for(let [ch, vl] of assuntosMap.entries()){
+    console.log(ch, vl)
+}
+
+const s = new Set(['a', 'b', 'c'])
+for (let letras of s){
+    console.log(letra)
+}
